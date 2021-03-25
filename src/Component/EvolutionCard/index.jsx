8c85a19a56521deb40c9baeb324426bbf396d060
glanglois97 <32880414+glanglois97@users.noreply.github.com>
2021-03-24 20:23:50 -0400

    Fixing empty box

--- src/Component/EvolutionCard/index.jsx
+++ src/Component/EvolutionCard/index.jsx
@@ -7,22 +7,31 @@ const EvolutionCard = ({ pokemonName, selected }) => {
   useEffect(() => {
     pokemonName &&
       fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(response =>
-        response.json().then(pokemon => {
-          setPokemon(pokemon);
-        })
+        response
+          .json()
+          .then(pokemon => {
+            setPokemon(pokemon);
+          })
+          .catch(error => {
+            setPokemon(null);
+          })
       );
   }, [pokemonName]);
   return (
-    <div className={`evolution-card ${selected ? "selected" : ""}`}>
-      <Link className="link" to={`${pokemon?.name}`}>
-        <h4 className="evolution-name">{pokemon?.name?.toUpperCase()}</h4>
-        <img
-          className="evolution-pokemon-sprite"
-          src={pokemon?.sprites?.front_default}
-          alt={pokemon?.name}
-        />
-      </Link>
-    </div>
+    <>
+      {pokemon && (
+        <div className={`evolution-card ${selected ? "selected" : ""}`}>
+          <Link className="link" to={`${pokemon?.name}`}>
+            <h4 className="evolution-name">{pokemon?.name?.toUpperCase()}</h4>
+            <img
+              className="evolution-pokemon-sprite"
+              src={pokemon?.sprites?.front_default}
+              alt={pokemon?.name}
+            />
+          </Link>
+        </div>
+      )}
+    </>
   );
 };
 export default EvolutionCard;
